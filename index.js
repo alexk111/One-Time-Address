2cd680bfa9e61c90c95fd49205f015c795dbfd9d
alexk111 <alexk111@users.noreply.github.com>
2019-08-25 20:30:00 +0700

    Init rendering

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
+const path = require('path')
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+
+const hbs = require('hbs')
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
 
 const app = express()
+app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use('/static', express.static('static', {
+  fallthrough: false
+}))
+
 app.get('/', (req, res) => {
-  res.send('This will show a standalone Donation page. ?w=WALLETID')
+  try {
+    const walletId = req.query.w || ''
+    const walletData = {}
+    res.render('pages/index.hbs', {
+      walletId,
+      walletData
+    })
+  } catch (e) {
+    res.status(500).render('pages/error.hbs')
+  }
 })
 
 app.get('/next-address.json', (req, res) => {
-  res.send('This is for getting a next Bitcoin address in JSON. ?w=WALLETID')
+  try {
+    const walletId = req.query.w || ''
+    const walletData = {}
+    res.json({ address: '1234567890' })
+  } catch (e) {
+    res.status(500).json({ error: true })
+  }
 })
 
 app.get('/widget', (req, res) => {
-  res.send('This will show Widget page for embedding via IFrame. ?w=WALLETID')
+  try {
+    const walletId = req.query.w || ''
+    const walletData = {}
+    res.render('pages/widget.hbs', {
+      walletId,
+      walletData
+    })
+  } catch (e) {
+    res.status(500).render('pages/error.hbs')
+  }
 })
 
 const SERVER_PORT = process.env.SERVER_PORT || 4000
