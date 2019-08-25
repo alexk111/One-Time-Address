f8210940969fa8cdc3a0021fb863caa8128bd4e7
alexk111 <alexk111@users.noreply.github.com>
2019-08-25 21:31:50 +0700

    Url reorg

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const path = require('path')
 const express = require('express')
-const bodyParser = require('body-parser')
 
 const hbs = require('hbs')
 hbs.registerPartials(path.join(__dirname, '/views/partials'))
 
 const app = express()
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
 
-app.use('/static', express.static('static', {
-  fallthrough: false
-}))
+app.use('', express.static('static'))
 
-app.get('/', (req, res) => {
+app.get('/widget/:walletId', (req, res) => {
   try {
-    const walletId = req.query.w || ''
+    const walletId = req.params.walletId || ''
     const walletData = {}
-    res.render('pages/index.hbs', {
+    res.render('pages/widget.hbs', {
       walletId,
       walletData
     })
@@ -27,21 +22,21 @@ app.get('/', (req, res) => {
   }
 })
 
-app.get('/next-address.json', (req, res) => {
+app.get('/json/:walletId', (req, res) => {
   try {
-    const walletId = req.query.w || ''
+    const walletId = req.params.walletId || ''
     const walletData = {}
-    res.json({ address: '1234567890' })
+    res.json({ address: '1234567890', walletId })
   } catch (e) {
     res.status(500).json({ error: true })
   }
 })
 
-app.get('/widget', (req, res) => {
+app.get('/:walletId', (req, res) => {
   try {
-    const walletId = req.query.w || ''
+    const walletId = req.params.walletId || ''
     const walletData = {}
-    res.render('pages/widget.hbs', {
+    res.render('pages/index.hbs', {
       walletId,
       walletData
     })
