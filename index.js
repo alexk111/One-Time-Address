4590fa6b4cf874fe4f8a81f0ced921e9a15219f4
alexk111 <alexk111@users.noreply.github.com>
2020-01-11 19:30:42 +0700

    Add option for transparent bg in widget mode

--- index.js
+++ index.js
@@ -1,74 +1,75 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const requestIp = require('request-ip')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const requestIp = require("request-ip");
 
-const initWallets = require('./modules/wallets')
+const initWallets = require("./modules/wallets");
 
-async function main () {
-  const wallets = await initWallets(true)
+async function main() {
+  const wallets = await initWallets(true);
 
-  hbs.registerPartials(path.join(__dirname, '/views/partials'))
+  hbs.registerPartials(path.join(__dirname, "/views/partials"));
 
-  const app = express()
-  app.set('view engine', 'hbs')
+  const app = express();
+  app.set("view engine", "hbs");
 
-  app.use('', express.static('static'))
+  app.use("", express.static("static"));
 
-  app.get('/:walletId/widget', (req, res) => {
+  app.get("/:walletId/widget", (req, res) => {
     try {
-      const walletId = req.params.walletId || 'default'
-      const wallet = wallets[walletId] || wallets['default']
+      const walletId = req.params.walletId || "default";
+      const wallet = wallets[walletId] || wallets["default"];
       if (wallet) {
-        res.render('pages/widget.hbs', {
-          walletId
-        })
+        res.render("pages/widget.hbs", {
+          walletId,
+          isTransparentBg: !!req.query.isTransparentBg
+        });
       } else {
-        res.status(404).render('pages/404.hbs')
+        res.status(404).render("pages/404.hbs");
       }
     } catch (e) {
-      res.status(500).render('pages/500.hbs')
+      res.status(500).render("pages/500.hbs");
     }
-  })
+  });
 
-  app.get('/:walletId.json', [requestIp.mw()], (req, res) => {
+  app.get("/:walletId.json", [requestIp.mw()], (req, res) => {
     try {
-      const walletId = req.params.walletId || 'default'
-      const wallet = wallets[walletId] || wallets['default']
-      const address = wallet.generateAddress(req.clientIp)
+      const walletId = req.params.walletId || "default";
+      const wallet = wallets[walletId] || wallets["default"];
+      const address = wallet.generateAddress(req.clientIp);
       if (wallet) {
-        res.json({ address, walletId })
+        res.json({ address, walletId });
       } else {
-        res.status(404).json({ error: true })
+        res.status(404).json({ error: true });
       }
     } catch (e) {
-      res.status(500).json({ error: true })
+      res.status(500).json({ error: true });
     }
-  })
+  });
 
-  app.get('/:walletId?', (req, res) => {
+  app.get("/:walletId?", (req, res) => {
     try {
-      const walletId = req.params.walletId || 'default'
-      const wallet = wallets[walletId] || wallets['default']
+      const walletId = req.params.walletId || "default";
+      const wallet = wallets[walletId] || wallets["default"];
       if (wallet) {
-        res.render('pages/page.hbs', {
+        res.render("pages/page.hbs", {
           walletId,
           pageData: wallet.page
-        })
+        });
       } else {
-        res.status(404).render('pages/404.hbs')
+        res.status(404).render("pages/404.hbs");
       }
     } catch (e) {
-      res.status(500).render('pages/500.hbs')
+      res.status(500).render("pages/500.hbs");
     }
-  })
+  });
 
-  const SERVER_PORT = process.env.SERVER_PORT || 4000
-  const SERVER_HOST = process.env.SERVER_HOST || 'localhost'
+  const SERVER_PORT = process.env.SERVER_PORT || 4000;
+  const SERVER_HOST = process.env.SERVER_HOST || "localhost";
 
   app.listen(SERVER_PORT, SERVER_HOST, () => {
-    console.log(`*** I'm listening on ${SERVER_HOST}:${SERVER_PORT} ***`)
-  })
+    console.log(`*** I'm listening on ${SERVER_HOST}:${SERVER_PORT} ***`);
+  });
 }
 
-main()
+main();
